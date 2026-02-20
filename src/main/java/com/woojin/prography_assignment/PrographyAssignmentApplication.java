a30d22a1da08c9c271c3e5f17ad2d2203ae9b71a
woogym <kwj9294@gmail.com>
2026-02-20 17:30:39 +0900

    chore: jpa auditing 추가

--- src/main/java/com/woojin/prography_assignment/PrographyAssignmentApplication.java
+++ src/main/java/com/woojin/prography_assignment/PrographyAssignmentApplication.java
@@ -2,10 +2,11 @@ package com.woojin.prography_assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PrographyAssignmentApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PrographyAssignmentApplication.class, args);
     }
