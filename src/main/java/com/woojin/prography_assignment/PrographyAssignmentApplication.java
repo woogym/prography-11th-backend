e3da277e4df55cfb890d29afba35b6b6876ad016
woogym <kwj9294@gmail.com>
2026-02-20 15:31:27 +0900

    intial commit' ;

---
+++ src/main/java/com/woojin/prography_assignment/PrographyAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.woojin.prography_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrographyAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrographyAssignmentApplication.class, args);
+    }
+
+}
