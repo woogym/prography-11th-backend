0a2ea8d4b8d72700a0864be18264d8eb7d42ae73
woogym <kwj9294@gmail.com>
2026-02-24 04:24:41 +0900

    feat: 제약 설정 추가

--- src/main/java/com/woojin/prography_assignment/member/dto/MemberCreateRequest.java
+++ src/main/java/com/woojin/prography_assignment/member/dto/MemberCreateRequest.java
@@ -1,12 +1,24 @@
 package com.woojin.prography_assignment.member.dto;
 
 import com.woojin.prography_assignment.member.domain.Member;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record MemberCreateRequest(
+
+        @NotBlank(message = "로그인 ID는 필수입니다.")
         String loginId,
+        @NotBlank(message = "비밀번호는 필수입니다.")
         String password,
+        @NotBlank(message = "이름은 필수입니다.")
         String name,
+        @Pattern(
+                regexp = "^01[0-9]-\\d{3,4}-\\d{4}$",
+                message = "전화번호 형식이 올바르지 않습니다."
+        )
+        @NotBlank(message = "전화번호는 필수입니다.")
         String phone,
+        @NotBlank(message = "기수 Id는 필수입니다.")
         Long cohortId,
         Long partId,
         Long teamId
