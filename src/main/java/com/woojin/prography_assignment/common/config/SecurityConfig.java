f3cc96388d42402141c6de8e226e598eadf83223
woogym <kwj9294@gmail.com>
2026-02-24 05:40:02 +0900

    feat: http 모든 요청 허용 설정 추가

--- src/main/java/com/woojin/prography_assignment/common/config/SecurityConfig.java
+++ src/main/java/com/woojin/prography_assignment/common/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.woojin.prography_assignment.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12); // 시간 남으면 환경변수로 처리하기.
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+
+        return http.build();
     }
 }
