20b9cee37f6751e6c0af1daa9c048b622e4547dc
woogym <kwj9294@gmail.com>
2026-02-23 18:34:41 +0900

    feat: 파트 도메인 추가

---
+++ src/main/java/com/woojin/prography_assignment/cohort/domain/Part.java
@@ -0,0 +1,43 @@
+package com.woojin.prography_assignment.cohort.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Part {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "part_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "part_type", nullable = false, foreignKey = @ForeignKey(name = "fk_part_cohort"))
+    private Cohort cohort;
+
+    @Enumerated(EnumType.STRING)
+    private PartType partType;
+
+    private Part(Cohort cohort, PartType partType) {
+        this.cohort = cohort;
+        this.partType = partType;
+    }
+
+    public static Part create(Cohort cohort, PartType partType) {
+        return new Part(cohort, partType);
+    }
+}
