a8abfd72d3f8e45b389c6342595e5cef7e9723eb
woogym <kwj9294@gmail.com>
2026-02-23 23:20:43 +0900

    feat: 멤버 레포지토리 추가

---
+++ src/main/java/com/woojin/prography_assignment/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.woojin.prography_assignment.member.repository;
+
+import com.woojin.prography_assignment.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
