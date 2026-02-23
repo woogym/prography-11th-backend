64f50401389542d043a553b156937b49452922cf
woogym <kwj9294@gmail.com>
2026-02-24 04:21:44 +0900

    chore: passwordEncoder bean 주입 config 추가

---
+++ src/main/java/com/woojin/prography_assignment/common/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.woojin.prography_assignment.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12); // 시간 남으면 환경변수로 처리하기.
+    }
+}
