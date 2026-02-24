34c926dcb148adb5e2c73603526f00a7216828f5
woogym <kwj9294@gmail.com>
2026-02-24 23:46:08 +0900

    feat: 회원 단건 조회 api 추가

---
+++ src/main/java/com/woojin/prography_assignment/member/controller/MemberController.java
@@ -0,0 +1,32 @@
+package com.woojin.prography_assignment.member.controller;
+
+import com.woojin.prography_assignment.common.dto.ApiResponse;
+import com.woojin.prography_assignment.member.dto.response.MemberResponse;
+import com.woojin.prography_assignment.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<MemberResponse>> getMember(@PathVariable Long id) {
+
+        MemberResponse response = memberService.getMember(id);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success(response));
+    }
+}
---
+++ src/main/java/com/woojin/prography_assignment/member/service/MemberService.java
@@ -0,0 +1,28 @@
+package com.woojin.prography_assignment.member.service;
+
+import com.woojin.prography_assignment.common.exception.ErrorCode;
+import com.woojin.prography_assignment.common.exception.model.BusinessException;
+import com.woojin.prography_assignment.member.domain.Member;
+import com.woojin.prography_assignment.member.dto.response.MemberCreateResponse;
+import com.woojin.prography_assignment.member.dto.response.MemberResponse;
+import com.woojin.prography_assignment.member.repository.MemberRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public MemberResponse getMember(Long id) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND,
+                        ErrorCode.MEMBER_NOT_FOUND.getMessage()));
+
+        return MemberResponse.from(member);
+    }
+}
