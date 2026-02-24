bd8070014b92eb96c54f0f81d2f301eb4e899be0
woogym <kwj9294@gmail.com>
2026-02-25 00:04:35 +0900

    style: partType 생성 순서별 번호 주석 추가

--- src/main/java/com/woojin/prography_assignment/cohort/domain/PartType.java
+++ src/main/java/com/woojin/prography_assignment/cohort/domain/PartType.java
@@ -1,9 +1,9 @@
 package com.woojin.prography_assignment.cohort.domain;
 
 public enum PartType {
-    SERVER,
-    WEB,
-    IOS,
-    ANDROID,
-    DESIGN
+    SERVER, // 1
+    WEB, // 2
+    IOS, // 3
+    ANDROID, // 4
+    DESIGN // 5
 }
