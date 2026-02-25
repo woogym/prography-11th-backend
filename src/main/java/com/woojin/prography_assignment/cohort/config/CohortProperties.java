17cacfcf33d16f28b240f89e49952a4292c6fa44
woogym <kwj9294@gmail.com>
2026-02-25 21:55:33 +0900

    fix: setter 누락으로 인한 바인딩 실패를 해결

--- src/main/java/com/woojin/prography_assignment/cohort/config/CohortProperties.java
+++ src/main/java/com/woojin/prography_assignment/cohort/config/CohortProperties.java
@@ -1,10 +1,12 @@
 package com.woojin.prography_assignment.cohort.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "current-cohort")
 public class CohortProperties {
