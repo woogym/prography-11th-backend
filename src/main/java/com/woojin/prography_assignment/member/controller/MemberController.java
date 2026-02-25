9b0befeda3d2d9ed7dfddc315097e909f67694a0
woogym <kwj9294@gmail.com>
2026-02-26 07:40:52 +0900

    fix: http method 수정

--- src/main/java/com/woojin/prography_assignment/member/controller/MemberController.java
+++ src/main/java/com/woojin/prography_assignment/member/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class MemberController {
                 .body(ApiResponse.success(response));
     }
 
-    @PatchMapping("/admin/members/{id}")
+    @PutMapping("/admin/members/{id}")
     public ResponseEntity<ApiResponse<MemberDetailResponse>> updateMember(@PathVariable Long id,
                                                                           @RequestBody MemberUpdateRequest request) {
         MemberDetailResponse response = memberService.updateMember(id, request);
